2daccbdfb2d105b5db3c6b49b92d22a77d6888ae
sunanda35 <sunandasamanta38@gmail.com>
2021-05-08 16:51:03 +0530

    update

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+const { response } = require('express');
+var express = require('express');
+var compile = require('./compile')
 var app = express()
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
 var PORT = 3000
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
+app.post('/call', async(req, res) => {
+    try {
+        await compile(req.body.code, req.body.language).then(response => {
+            console.log(response)
+            res.send(response)
+    })
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 app.listen(PORT, () => {
     console.log('http://localhost:3000')
+    console.log('http://localhost:3000/call')
 })
