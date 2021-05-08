f11f26d737818e85ea78097859691720ca06a1ad
sunanda35 <sunandasamanta38@gmail.com>
2021-05-08 16:54:20 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { response } = require('express');
 var express = require('express');
-var compile = require('./compile')
 var app = express()
 app.use(express.json());
 app.use(express.urlencoded({
@@ -13,16 +12,9 @@ var PORT = 3000
 app.get('/', (req, res) => {
     res.send('hello world')
 })
-app.post('/call', async(req, res) => {
-    try {
-        await compile(req.body.code, req.body.language).then(response => {
-            console.log(response)
-            res.send(response)
-    })
-    } catch (error) {
-        console.log(error)
-    }
-})
+// app.post('/call', async(req, res) => {
+    
+// })
 
 app.listen(PORT, () => {
     console.log('http://localhost:3000')
