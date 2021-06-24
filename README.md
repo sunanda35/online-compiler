bc924e8bb9fa0dbb72537309bfb6e94603d5635d
sunanda35 <sunandasamanta38@gmail.com>
2021-06-24 14:31:26 +0530

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# It's a Online Compiler
+
+In backend folder, all backend part to execute code and outcast using a api endpoint
+
+In frontend folder, website part which will execute part
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express"); //import express for create api
+const cors = require("cors"); // import cors for bypass cors error for same url domain
 require("dotenv").config(); //for get data from .env file
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT_OPEN || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
