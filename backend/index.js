f6ac7d8992db278ab31a5c051310337a070f2acf
sunanda35 <sunandasamanta38@gmail.com>
2021-06-24 14:57:56 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,26 @@ require("dotenv").config(); //for get data from .env file
 const app = express();
 const PORT = process.env.PORT_OPEN || 3000;
 
-app.use(cors());
+app.use(cors()); // bypass browser cors error
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
-);
+); // for all type of api req
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.send("It's a compiler backend part.");
+}); //test get api create
+app.post("/api/compile/", (req, res, next) => {
+  const language = req.body.language;
+  const code = req.body.code;
+  res.send({
+    language: language,
+    code: code,
+  });
 });
 
-app.listen(PORT, () => console.log(`Port open at: http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Port open at: http://localhost:${PORT}`);
+}); // create a server using port
