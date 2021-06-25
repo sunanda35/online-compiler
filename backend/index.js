0a826f6a4a53c86d938c09f008612f69ce5c29fe
sunanda35 <sunandasamanta38@gmail.com>
2021-06-25 18:03:33 +0530

    partially done

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 const express = require("express"); //import express for create api
+const fs = require("fs"); //import fs for file manupulate
+const { spawn } = require("child_process");
 const cors = require("cors"); // import cors for bypass cors error for same url domain
+const { stdout } = require("process");
 require("dotenv").config(); //for get data from .env file
 
 const app = express();
@@ -11,18 +14,52 @@ app.use(
   express.urlencoded({
     extended: true,
   })
-); // for all type of api req
+); // for all type of api request
 
 app.get("/api/", (req, res) => {
   res.send("It's a compiler backend part.");
 }); //test get api create
-app.post("/api/compile/", (req, res, next) => {
+
+app.post("/api/compile/", async (req, res, next) => {
   const language = req.body.language;
-  const code = req.body.code;
-  res.send({
-    language: language,
-    code: code,
+  const code = `#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"OOO mother chod";
+    return 0;
+}`;
+  fs.writeFileSync("./utils/helo.cpp", code, (err, data) => {
+    if (err) console.log(err);
+    console.log(data);
   });
+
+  let results, error;
+  var ls = await spawn("g++ ./utils/helo.cpp && ./a.out", { shell: true });
+
+  ls.stdout.on("data", (data) => {
+    results = `${data}`;
+  }); //output of c++ code
+
+  ls.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+    error = `${data}`;
+  }); //error of code
+  ls.on("error", (error) => {
+    error = `${data}`;
+  }); //error
+
+  // ls.stdin.pipe(res);
+
+  ls.on("close", (close) => {
+    res.status(200).json({
+      language: language,
+      code: code,
+      output: results,
+      err: error,
+    });
+  }); //completed code execution
 });
 
 app.listen(PORT, () => {
---
+++ backend/utils/helo.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"OOO mother chod";
+    return 0;
+}
